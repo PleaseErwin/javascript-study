0c278f7f3b53a636ac94869a013767474bd28d24
PleaseErwin <sowon6257@gmail.com>
2022-08-19 13:16:26 +0900

    배경이미지 지정 및 css 수정

--- js/background.js
+++ js/background.js
@@ -1,4 +1,4 @@
 const images = ["0.jpg", "1.jpg", "2.jpg", "3.jpg"];
 const todaysImage = images[Math.floor(Math.random() * images.length)];
 
-document.body.style.backgroundImage = `url(img/${todaysImage})`;
+document.body.style.backgroundImage = `url(img/0.jpg)`;//${todaysImage}
